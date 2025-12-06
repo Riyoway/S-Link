340658492a0935be0b1e5666ab25360a6c09d64a
Riyo <ryohsg@gmail.com>
2025-12-07 01:43:29 +0900

    test

--- components/background-animation.tsx
+++ components/background-animation.tsx
@@ -13,7 +13,7 @@ export default function BackgroundAnimation() {
   const { View } = useLottie(options);
 
   return (
-    <div className="fixed inset-0 -z-10 h-full w-full opacity-50 scale-[3] sm:scale-100 pointer-events-none">
+    <div className="fixed left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 w-[300vw] h-[300vh] sm:w-full sm:h-full opacity-50 pointer-events-none">
       {View}
     </div>
   );
